961bc6911917918a1e1314ab7ee1adec757b96e1
Yaroslav Burakov <yarikbur@yandex.ru>
2022-05-20 19:20:17 +0300

    Update FastSorting.java

--- src/ru/asfick/methods/FastSorting.java
+++ src/ru/asfick/methods/FastSorting.java
@@ -42,12 +42,12 @@ public class FastSorting implements SortingMethod {
 	@Override
 	public void sort(int[] array) {
 		
-		System.out.print('\n');
-		SortingMethod.writeArray(array);
+		//System.out.print('\n');
+		//SortingMethod.writeArray(array);
 		
 		impl(array, 0, array.length - 1);
 		
-		SortingMethod.writeArray(array);
+		//SortingMethod.writeArray(array);
 	}
 
 }
